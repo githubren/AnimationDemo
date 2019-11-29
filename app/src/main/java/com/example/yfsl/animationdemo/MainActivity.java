00f38387c2357acdb2abc191de3c75135b3d6166
renbing <970910609@qq.com>
2019-11-29 13:49:49 +0800

    增加组合动画

--- app/src/main/java/com/example/yfsl/animationdemo/MainActivity.java
+++ app/src/main/java/com/example/yfsl/animationdemo/MainActivity.java
@@ -1,8 +1,10 @@
 package com.example.yfsl.animationdemo;
 
 import android.animation.Animator;
+import android.animation.AnimatorSet;
 import android.animation.ObjectAnimator;
 import android.animation.PropertyValuesHolder;
+import android.animation.ValueAnimator;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -22,6 +24,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     private Button mRotationAni;
     private Button mAlphaAni;
     private Button mCombinationAni;
+    private Button mCombinationOrderAni;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -40,6 +43,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         mRotationAni.setOnClickListener(this);
         mAlphaAni.setOnClickListener(this);
         mCombinationAni.setOnClickListener(this);
+        mCombinationOrderAni.setOnClickListener(this);
     }
 
     private void initView() {
@@ -47,9 +51,13 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         mScaleAni = findViewById(R.id.scale_animation);
         mRotationAni = findViewById(R.id.rotation_animation);
         mAlphaAni = findViewById(R.id.alpha_animation);
-        mCombinationAni = findViewById(R.id.combination_animation);
+        mCombinationAni = findViewById(R.id.combination_animation_together);
+        mCombinationOrderAni = findViewById(R.id.combination_animation_order);
     }
 
+    /**
+     * 平移动画
+     */
     private void startTranslationAnimation(){
         //创建动画对象  第一个参数是需要执行动画的目标；第二个参数是动画的类型；第三个是动画执行的参数
         ObjectAnimator animator = ObjectAnimator.ofFloat(mTranslateAni,"translationX",400);
@@ -83,6 +91,9 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         });
     }
 
+    /**
+     * 缩放动画
+     */
     private void startScaleAnimation(){
         ObjectAnimator animator = ObjectAnimator.ofFloat(mScaleAni,"scaleX",1.0f,0.3f);
         animator.setDuration(1000);
@@ -114,6 +125,9 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         });
     }
 
+    /**
+     * 旋转动画
+     */
     private void startRotationAnimation(){
         ObjectAnimator animator = ObjectAnimator.ofFloat(mRotationAni,"rotation",360);
         animator.setDuration(1000);
@@ -121,6 +135,9 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         animator.start();
     }
 
+    /**
+     * 透明度动画
+     */
     private void startAlphaAnimation(){
         ObjectAnimator animator = ObjectAnimator.ofFloat(mAlphaAni,"alpha",1f,0.3f);
         animator.setDuration(1000);
@@ -152,14 +169,45 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         });
     }
 
+    /**
+     * 组合动画
+     */
     private void startCombinationAnimation(){
-        PropertyValuesHolder holderTranslationX = PropertyValuesHolder.ofFloat("translationX",0);
-        PropertyValuesHolder holderTranslationY = PropertyValuesHolder.ofFloat("translationY",0);
-        PropertyValuesHolder holderScaleX = PropertyValuesHolder.ofFloat("scaleX",1,0);
-        PropertyValuesHolder holderScaleY = PropertyValuesHolder.ofFloat("scaleY",1,0);
-        PropertyValuesHolder holderRotation = PropertyValuesHolder.ofFloat("rotation",360);
-        PropertyValuesHolder holderAlpha = PropertyValuesHolder.ofFloat("alpha",1,0);
+        PropertyValuesHolder holderTranslationX = PropertyValuesHolder.ofFloat("translationX",0);//X方向平移为0
+        PropertyValuesHolder holderTranslationY = PropertyValuesHolder.ofFloat("translationY",0);//Y方向平移为0
+        PropertyValuesHolder holderScaleX = PropertyValuesHolder.ofFloat("scaleX",1,0);//X方向缩放
+        PropertyValuesHolder holderScaleY = PropertyValuesHolder.ofFloat("scaleY",1,0);//Y方向缩放
+        PropertyValuesHolder holderRotation = PropertyValuesHolder.ofFloat("rotation",360);//旋转
+        PropertyValuesHolder holderAlpha = PropertyValuesHolder.ofFloat("alpha",1,0);//透明度
         ObjectAnimator.ofPropertyValuesHolder(mCombinationAni,holderTranslationX,holderTranslationY,holderScaleX,holderScaleY,holderRotation,holderAlpha).setDuration(1000).start();
+    }
+
+    /**
+     * 先后顺序的组合动画
+     */
+    private void startCombinationOrderAnimation(){
+        AnimatorSet set = new AnimatorSet();
+        //平移动画
+        ObjectAnimator animatorTranslation = ObjectAnimator.ofFloat(mCombinationOrderAni,"translationX",400);
+        animatorTranslation.setDuration(1000);
+        animatorTranslation.setInterpolator(new DecelerateInterpolator());
+        //缩放动画
+        ObjectAnimator animatorScale = ObjectAnimator.ofFloat(mCombinationOrderAni,"scaleX",1f,0.3f);
+        animatorScale.setDuration(1000);
+        animatorScale.setInterpolator(new DecelerateInterpolator());
+        //旋转动画
+        ObjectAnimator animatorRotation = ObjectAnimator.ofFloat(mCombinationOrderAni,"rotation",360);
+        animatorRotation.setDuration(1000);
+        animatorRotation.setInterpolator(new DecelerateInterpolator());
+        //透明度动画
+        ObjectAnimator animatorAlpha = ObjectAnimator.ofFloat(mCombinationOrderAni,"alpha",1f,0.3f);
+        animatorAlpha.setDuration(1000);
+        animatorAlpha.setInterpolator(new DecelerateInterpolator());
+        //设置动画的先后顺序
+        set.play(animatorTranslation).before(animatorScale);
+        set.play(animatorScale).before(animatorRotation);
+        set.play(animatorRotation).before(animatorAlpha);
+        set.start();
     }
 
     @Override
@@ -177,8 +225,11 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
             case R.id.alpha_animation:
                 startAlphaAnimation();
                 break;
-            case R.id.combination_animation:
+            case R.id.combination_animation_together:
                 startCombinationAnimation();
+                break;
+            case R.id.combination_animation_order:
+                startCombinationOrderAnimation();
                 break;
         }
     }
